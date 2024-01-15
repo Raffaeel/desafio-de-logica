e89f8df0f73ca6f3de2218c72cf9d6719d115a38
Raffaeel <142857378+Raffaeel@users.noreply.github.com>
2024-01-15 12:13:20 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// nome do heroi 
+// precisa pegar trez diamante e passar para  no nivel desejado
+//* niveis de xp : 1 á 2 ferro ,  3 á 4 bronze  ,   4 á 5 prata   5 á 6  ouruo   6 á 7 platina     
+// e por fim apresentar a menssagem
+
+let nomeDoHeroi = "darksaba"
+
+let xp=["1 á 2 ferro"] ["3 á 4 bronze"] ["4 á 5 prata"] ["5 á 6 ouro"] ["6 á 7 platina"]
+      let quantosDiamante= 3>=10.000 // 3 DIAMANEMTE É = A 10.000 XP NO NIVEL MAXIMO
+         let xpMAXIMO=10.000
+              console.log!=((quantosDiamante>=7))&&(xp)&&(xpMAXIMO===true) // comparei a quantidade de diamante se for mair que 3 é igual 10.00 de pontos entao esta no xp maximo ja pode avançar
+  
+              if(xpMAXIMO===7) {console.log({nomeDoHeroi}, +  "esta no nivel " + {xp})}
+   
+
+  // resumo linha 6(o nome do heroi é DARKSABA)
+  //linha 7 (ele precisava de 3 diamantes que seria equivalente a 10.00)
+  // linha 8 (o nivel maximo de xp que ele precisava para vencer seria 10.00)
+  // na linha 9 verifique  se nao fosse conresponde os valores que seja falso e se for que seja verdadeiro
+  // linha 10(foi feita a verificção  qual o nivel do heroi  )  
